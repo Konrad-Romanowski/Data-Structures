315aff85198ca9eac2ba3306398a1075caaace26
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2021-08-16 20:44:39 +0200

    Initialized BinaryHeap file

---
+++ BinaryHeap.js
@@ -0,0 +1,29 @@
+class MaxBinaryHeap {
+    constructor() {
+        this.data = [];
+    }
+
+    swapNodes(_index1,_index2) {
+        [this.data[_index1], this.data[_index2]] = [this.data[_index2],this.data[_index1]];
+    }
+
+    bubbleUp(_childIndex,_parentIndex) {
+        while(_childIndex > _parentIndex) {
+            if(this.data[_childIndex] > this.data[_parentIndex]) {
+                this.swapNodes(_childIndex,_parentIndex);
+            }
+            _childIndex = _parentIndex;
+            _parentIndex = Math.floor((_childIndex - 1)/2);
+        }
+    }
+
+    insert(_data) {
+        this.data.push(_data);
+        let childIndex = this.data.length - 1;
+        let parentIndex = Math.floor((childIndex - 1)/2);
+
+        this.bubbleUp(childIndex,parentIndex);
+
+        return this.data;
+    }
+}
