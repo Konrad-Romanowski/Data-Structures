85fcc19a904fd62be8e450954de87bb2b2e99603
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2021-08-17 19:50:19 +0200

    Refactored insert and bubbleUp methods

--- BinaryHeap.js
+++ BinaryHeap.js
@@ -7,20 +7,21 @@ class MaxBinaryHeap {
         [this.data[_index1], this.data[_index2]] = [this.data[_index2],this.data[_index1]];
     }
 
-    bubbleUp(_childIndex,_parentIndex) {
-        while(this.data[_childIndex] > this.data[_parentIndex]) {
-            this.swapNodes(_childIndex,_parentIndex);
-            _childIndex = _parentIndex;
-            _parentIndex = Math.floor((_childIndex - 1)/2);
+    bubbleUp(_childIndex) {
+        let parentIndex = Math.floor((_childIndex - 1)/2);;
+
+        while(this.data[_childIndex] > this.data[parentIndex]) {
+            this.swapNodes(_childIndex,parentIndex);
+            _childIndex = parentIndex;
+            parentIndex = Math.floor((_childIndex - 1)/2);
         }
     }
 
     insert(_data) {
         this.data.push(_data);
         let childIndex = this.data.length - 1;
-        let parentIndex = Math.floor((childIndex - 1)/2);
 
-        this.bubbleUp(childIndex,parentIndex);
+        this.bubbleUp(childIndex);
 
         return this.data;
     }
