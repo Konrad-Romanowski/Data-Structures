a6d85c64bb4512c7144f51f9481db2e203c4153c
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2021-08-18 20:11:40 +0200

    Added extractMax method (recursive and iterative implementationof boubbleDown method), added recursive boubbleUp method

--- BinaryHeap.js
+++ BinaryHeap.js
@@ -8,14 +8,70 @@ class MaxBinaryHeap {
     }
 
     bubbleUp(_childIndex) {
-        let parentIndex = Math.floor((_childIndex - 1)/2);;
+        let parentIndex = Math.floor((_childIndex - 1)/2);
 
-        while(this.data[_childIndex] > this.data[parentIndex]) {
+        if(this.data[_childIndex] > this.data[parentIndex]) {
             this.swapNodes(_childIndex,parentIndex);
-            _childIndex = parentIndex;
-            parentIndex = Math.floor((_childIndex - 1)/2);
+            this.bubbleUp(parentIndex);
         }
     }
+
+    //### iterative implementation  ###
+    // bubbleUp(_childIndex) {
+    //     let parentIndex = Math.floor((_childIndex - 1)/2);
+    
+    //     while(this.data[_childIndex] > this.data[parentIndex]) {
+    //         this.swapNodes(_childIndex,parentIndex);
+    //         _childIndex = parentIndex;
+    //         parentIndex = Math.floor((_childIndex - 1)/2);
+    //     }
+    // }
+
+    bubbleDown(_parentIndex) {
+        let leftChildIndex = 2 * _parentIndex + 1;
+        let rightChildIndex = 2 * _parentIndex + 2;
+        let childToSwapIndex;
+
+        if(leftChildIndex > this.data.length - 1) return;
+        if(rightChildIndex <= this.data.length - 1) {
+            childToSwapIndex = this.data[leftChildIndex] > this.data[rightChildIndex] ? leftChildIndex : rightChildIndex;
+        } else {
+            childToSwapIndex = leftChildIndex;
+        }
+
+        if(this.data[childToSwapIndex] > this.data[_parentIndex]) {
+            this.swapNodes(_parentIndex,childToSwapIndex);
+            this.bubbleDown(childToSwapIndex);
+        }
+    }
+
+    // ### iterative implementation ###
+    // bubbleDown(_parentIndex) {
+    //     let leftChildIndex = 2 * _parentIndex + 1;
+    //     let rightChildIndex = 2 * _parentIndex + 2;
+    //     let childToSwapIndex;
+
+    //     if(leftChildIndex > this.data.length - 1) return;
+    //     if(rightChildIndex <= this.data.length - 1) {
+    //         childToSwapIndex = this.data[leftChildIndex] > this.data[rightChildIndex] ? leftChildIndex : rightChildIndex;
+    //     } else {
+    //         childToSwapIndex = leftChildIndex;
+    //     }
+
+    //     while(this.data[childToSwapIndex] > this.data[_parentIndex]) {
+    //         this.swapNodes(_parentIndex,childToSwapIndex);
+    //         _parentIndex = childToSwapIndex 
+    //         leftChildIndex = 2 * _parentIndex + 1;
+    //         rightChildIndex = 2 * _parentIndex + 2;
+
+    //         if(leftChildIndex > this.data.length - 1) return;
+    //         if(rightChildIndex <= this.data.length - 1) {
+    //             childToSwapIndex = this.data[leftChildIndex] > this.data[rightChildIndex] ? leftChildIndex : rightChildIndex;
+    //         } else {
+    //             childToSwapIndex = leftChildIndex;
+    //         }
+    //     }
+    // }
 
     insert(_data) {
         this.data.push(_data);
@@ -24,5 +80,18 @@ class MaxBinaryHeap {
         this.bubbleUp(childIndex);
 
         return this.data;
+    }
+
+    extractMax() {
+        if (this.data.length === 0) return undefined;
+
+        const lastNode = this.data.length - 1;
+
+        this.swapNodes(0,lastNode);
+        const extractedRoot = this.data.pop();
+        
+        this.bubbleDown(0);
+
+        return extractedRoot;
     }
 }
