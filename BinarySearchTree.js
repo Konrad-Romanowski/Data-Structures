71f659eb9ae52e58570adf55026b8ae13c4e8515
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2021-10-06 20:54:24 +0200

    BST: added remove method

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -77,6 +77,45 @@ class BinarySearchTree {
         return currentNode.data;
     }
 
+    remove(_data) {
+        if(!this.root) return;
+
+        const removeNode = (node,data) => {
+            if(data === node.data) {
+                // if node has no children remove it by setting its data to null
+                if(!node.left && !node.right) {
+                    return null;
+                }
+                // if node has no left child remove it by setting it as whole right child
+                if(!node.left) {
+                    return node.right;
+                }
+                // if node has no right child remove it by setting it as whole left child
+                if(!node.right) {
+                    return node.left;
+                }
+                // if node has two children
+                // first go to the right child and then traverse all the way to the left leaf
+                let currentNode = node.right;
+                while(currentNode.left) {
+                    currentNode = currentNode.left;
+                }
+                // set the node data that we want to remove to the far left leaf we have found
+                node.data = currentNode.data;
+                // and call the removeNode on the right child to traverse it and remove the far left leaf
+                node.right = removeNode(node.right, currentNode.data);
+                return node;
+            } else if(data < node.data) {
+                node.left = removeNode(node.left,data);
+                return node;
+            } else {
+                node.right = removeNode(node.right,data);
+                return node;
+            }
+        }
+        this.root = removeNode(this.root, _data);
+    }
+
     //Breadth First Search
     BFS() {
         let treeItems = [];
